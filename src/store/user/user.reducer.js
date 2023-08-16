994ae93ff69ad3f88b6b22ec8bbbeb0c0507594f
Dark-Assualt <karanrjsgurjar@gmail.com>
2023-08-16 17:49:53 +0530

    Migrated tp Redux

---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,21 @@
+import{USER_ACTON_TYPES} from './user.types'
+
+  const INITIAL_STATE = {
+    currentUser : null
+  }
+  
+  export const userReducer = (state = INITIAL_STATE, action) => {
+    const {type, payload} = action;
+   
+    switch(type) {
+      case USER_ACTON_TYPES.SET_CURRENT_USER:
+        return{
+          ...state,
+          currentUser: payload
+        }
+  
+      default:
+        return state;
+  
+    }
+  }
